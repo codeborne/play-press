47df6433039644d3835b1b5ec46226aec93d6192
Anton Keks <anton@codeborne.com>
2014-05-13 12:37:45 +0300

    add support for precompiled less files on production

--- src/press/PlayLessEngine.java
+++ src/press/PlayLessEngine.java
@@ -36,6 +36,13 @@ public class PlayLessEngine {
      * Get the CSS for this less file either from the cache, or compile it.
      */
     public String get(File lessFile, boolean compress) {
+        if (Play.mode.isProd()) {
+          VirtualFile precompiled = VirtualFile.fromRelativePath(lessFile.getPath() + ".css");
+          if (precompiled != null && precompiled.exists()) {
+            return precompiled.contentAsString();
+          }
+        }
+
         try {
             String cacheKey = lessFile.getName() + "." + latestModified(lessFile);
             CompressedFile cachedFile = CompressedFile.create(cacheKey, PluginConfig.css.compressedDir);
